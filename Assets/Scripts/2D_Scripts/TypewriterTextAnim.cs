7dd8f76efa0219c1500fb3dab8534a4813528789
theodore.gowan@gmail.com <>
2025-08-28 19:04:40 -0300

    Added intro screen for campaign, currently disabled

---
+++ Assets/Scripts/2D_Scripts/TypewriterTextAnim.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using TMPro;
+
+public class TypewriterTextAnim : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textMesh;
+
+    [SerializeField] float charsPerSecond;
+
+    float startTime;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startTime = Time.time;
+        textMesh.maxVisibleCharacters = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh.maxVisibleCharacters = Mathf.RoundToInt(charsPerSecond * (Time.time - startTime));
+    }
+}
